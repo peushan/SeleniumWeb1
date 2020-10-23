2fe9da59d413c0b8ce0e8f8c6e14023b2cbbf465
Peushan Panagoda <peushanpanagoda@gmail.com>
2020-10-23 14:13:15 +1300

    Update BaseTest.java
    test

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -18,3 +18,70 @@ public class BaseTest {
 
     }
 }
+
+
+pipeline {
+    agent {
+        label "NZLO_Windows_UFT"
+    }
+    tools{
+        maven 'MavenForSelenium'
+        jdk 'JDKForSelenium'
+    }
+    stages {
+        stage('mvn clean'){
+            steps {
+                bat 'mvn clean'
+            }
+        }
+        stage('mvn test'){
+            steps {
+                catchError(buildResult:'SUCCESS', stageResult:'FAILURE'){
+                    bat "mvn -DsuiteXmlFile=${suite_xml_file} -DtestngReportsDirectory=target/test-results/initialrun -DextentReportsDir=/target/ExtentReports/ExtentReportResults_InitialRun.html test"
+                }
+            }
+        }
+        stage('retry failed tests'){
+            steps{
+                bat "mvn -DsuiteXmlFile=target/test-results/initialrun/testng-failed.xml -DtestngReportsDirectory=target/test-results/rerun -DextentReportsDir=/target/ExtentReports/ExtentReportResults_Rerun.html test"
+            }
+        }
+    }
+    post{
+        always{
+            publishHTML target: [
+                    allowMissing: false,
+                    alwaysLinkToLastBuild: false,
+                    keepAll: true,
+                    reportDir:'target/test-results/initialrun',
+                    reportFiles:'index.html',
+                    reportName:'TestNg Report Initial Run'
+                ]
+            publishHTML target: [
+                    allowMissing: false,
+                    alwaysLinkToLastBuild: false,
+                    keepAll: true,
+                    reportDir:'target/test-results/rerun',
+                    reportFiles:'index.html',
+                    reportName:'TestNg Report Re Run'
+                ]
+            publishHTML target: [
+                    allowMissing: false,
+                    alwaysLinkToLastBuild: false,
+                    keepAll: true,
+                    reportDir:'target/ExtentReports',
+                    reportFiles:'ExtentReportResults_InitialRun.html',
+                    reportName:'Extent Report Intitial Run'
+                ]
+            publishHTML target: [
+                allowMissing: false,
+                alwaysLinkToLastBuild: false,
+                keepAll: true,
+                reportDir:'target/ExtentReports',
+                reportFiles:'ExtentReportResults_Rerun.html',
+                reportName:'Extent Report ReRun'
+            ]
+        }
+    }
+
+}
